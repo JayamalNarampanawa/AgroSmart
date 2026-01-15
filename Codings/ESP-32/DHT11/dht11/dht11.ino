c378f0ec821ea3fb5df28c7066b0e058f657f145
JayamalNarampanawa <ajnarampanawa@gmail.com>
2026-01-15 21:22:07 +0530

    Proposal
    Proposal for the project

--- Codings/ESP-32/DHT11/dht11/dht11.ino
+++ Codings/ESP-32/DHT11/dht11/dht11.ino
@@ -1,9 +1,9 @@
 void setup() {
-  // put your setup code here, to run once:
-
+  Serial.begin(115200);
+  Serial.println("ESP32 Power OK");
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  Serial.println("Running...");
+  delay(1000);
 }
