a928f91cd84de4ed9c637830e62bce64c33959ab
JayamalNarampanawa <ajnarampanawa@gmail.com>
2026-01-30 21:02:56 +0530

    Hosted
    Hosted the dashboard

--- dashboard/vite.config.js
+++ dashboard/vite.config.js
@@ -2,5 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  // GitHub Pages subpath for this repo
+  base: '/agrosmart-dashboard/',
   plugins: [react()],
 })
