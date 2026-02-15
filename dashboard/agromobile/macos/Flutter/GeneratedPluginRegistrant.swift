f1a29eaae5f3f64779f57cbb7a5cf20c40fecf8f
Mihiranga Rathnayake <mihirangarathnayake2006@gmail.com>
2026-02-16 00:21:28 +0530

    flutter daemon fixed

--- dashboard/agromobile/macos/Flutter/GeneratedPluginRegistrant.swift
+++ dashboard/agromobile/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,14 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import firebase_core
-import firebase_database
-import flutter_local_notifications
-import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
-  FLTFirebaseDatabasePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseDatabasePlugin"))
-  FlutterLocalNotificationsPlugin.register(with: registry.registrar(forPlugin: "FlutterLocalNotificationsPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
